3d1839fef19c8cea2f6173037561309ee337a710
Tapani <t8sima01@students.oamk.fi>
2020-04-15 11:45:38 +0300

    Lisaa_Alitehtava päivitetty, Gsonia aloitettu Lisaa_Tehtava luokassa

--- Mobiilisovellusprojekti/mobiilisovellusprojekti_v6/app/src/main/java/com/example/mobiilisovellus/Lisaa_Alitehtava.java
+++ Mobiilisovellusprojekti/mobiilisovellusprojekti_v6/app/src/main/java/com/example/mobiilisovellus/Lisaa_Alitehtava.java
@@ -2,7 +2,6 @@ package com.example.mobiilisovellus;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,9 +9,7 @@ public class Lisaa_Alitehtava extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_Lisaa_Alitehtava);
-
-
+        setContentView(R.layout.activity_lisaa_alitehtava);
     }
 
 
